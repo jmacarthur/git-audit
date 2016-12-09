5d4e8cf19ddbc4dd4b4de2b3fef58055c4e15956
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-12-09 15:07:01 +0000

    Change prints to logging statements

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import pygit2
 import sys
@@ -10,27 +11,28 @@ ERROR = 2
 
 issues = {}
 
-def record_issue(issue):
+def record_issue(issue, more_data):
     if issue in issues:
-        issues[issue] += 1
+        if more_data not in issues[issue]:
+            issues[issue].append(more_data)
     else:
-        issues[issue] = 1
+        issues[issue] = [more_data]
 
 def check_can_merge_to_this_branch(email):
     pass
 
 def track_back_to_parents(this_commit, excluded_author, all_baselines, depth = 1):
-    print("Checking feature branch %s by %s"%(this_commit.id, this_commit.author.email))
+    logging.debug("Checking feature branch %s by %s"%(this_commit.id, this_commit.author.email))
     if depth > 20:
-        print("Max depth exceeded while looking for the parents of %r"%this_commit)
+        logging.info("Max depth exceeded while looking for the parents of %r"%this_commit)
         sys.exit(ERROR)
-    print("Looking for parents of %r"%this_commit.message)
+    logging.debug("Looking for parents of %r"%this_commit.message)
     if this_commit.id in all_baselines:
-        print("Tracked back to base, nothing futher to do")
+        logging.debug("Tracked back to base, nothing futher to do")
         return None
     if this_commit.author.email == excluded_author:
-        print("This feature branch contains commit %s, authored by %s, who performed the merge. (%s)"%(this_commit.id, this_commit.author.email, excluded_author))
-        record_issue("Feature branch merged by one of its contributors")
+        logging.info("This feature branch contains commit %s, authored by %s, who performed the merge. (%s)"%(this_commit.id, this_commit.author.email, excluded_author))
+        record_issue("Feature branch merged by one of its contributors", this_commit.id)
         return None
     for p in this_commit.parents:
         return track_back_to_parents(p, excluded_author, all_baselines, depth + 1)
@@ -46,10 +48,10 @@ def find_baselines(commit):
             baselines.append(commit.id)
             commit = commit.parents[0]
         elif len(commit.parents) == 0:
-            print "Reached the end of the branch"
+            logging.debug("Reached the end of the branch")
             return baselines
         else:
-            record_issue("Non-merge commit on trunk")
+            record_issue("Non-merge commit on trunk", commit.id)
             baselines.append(commit.id)
             commit = commit.parents[0]
 
@@ -59,7 +61,7 @@ def main():
         sys.exit(ERROR)
     gitdir = sys.argv[1]
     if not os.path.isdir(gitdir):
-        print("%s is not a directory"%gitdir)
+        logging.error("%s is not a directory"%gitdir)
         sys.exit(ERROR)
     try:
         repo = pygit2.Repository(gitdir)
@@ -77,17 +79,17 @@ def main():
             if len(parents)==0:
                 # We've reached the end of the repository
                 break
-            
+
             if len(parents)<2:
-                record_issue("Non-merge commit on trunk.")
+                record_issue("Non-merge commit on trunk", commit.id)
                 commit = parents[0]
                 continue
-            
+
             check_can_merge_to_this_branch(merge_author)
 
             # ASSUME (!) the first parent was the previous position of the branch...
             feature_branches = parents[1:]
-            print("Base of this merge is %s"%(parents[0].id))
+            logging.debug("Base of this merge is %s"%(parents[0].id))
             for p in feature_branches:
                 ident = track_back_to_parents(p, merge_author, all_baselines)
             commit = parents[0]
@@ -101,8 +103,13 @@ def main():
     else:
         print("Issues found in git repository: ")
         for (k,v) in issues.items():
-            print("  %s (%d counts)"%(k,v))
-            sys.exit(AUDIT_FAILED)
+            if len(v) <= 5:
+                print("  %s (%d counts)"%(k,len(v)))
+                for note in v:
+                    print("    %s"%note)
+            else:
+                print("  %s (%d counts, not listed)"%(k,len(v)))
+        sys.exit(AUDIT_FAILED)
 
 
 if __name__=="__main__": main()
